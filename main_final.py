99cc9865cfd111d98ea8899bff8069dde277afba
piotrmp <piotr.m.przybyla@gmail.com>
2023-05-04 15:41:55 +0200

    Implementation of the final scoring.

---
+++ main_final.py
@@ -0,0 +1,189 @@
+import numpy as np
+import pathlib
+import random
+import sys
+import torch
+from torch.optim import Adam
+from torch.optim.lr_scheduler import MultiStepLR
+from torch.utils.data import TensorDataset, DataLoader
+from transformers import RobertaTokenizer
+from features.probabilistic import ProbabilisticFeatures, fixed_len
+from models.bilstm import BiLSTM
+from models.hybrid import HybridBiLSTMRoBERTa
+from models.training import eval_loop, train_loop
+
+random.seed(10)
+
+language = 'en'
+task = 'subtask_1'
+if len(sys.argv) == 3:
+    language = sys.argv[1]
+    task = sys.argv[2]
+
+# model_type = 'BiLSTM'
+model_type = 'Hybrid'
+disable_sequence = False
+
+if language == 'en':
+    roberta_variant = "roberta-base"
+elif language == 'es':
+    roberta_variant = 'PlanTL-GOB-ES/roberta-base-bne'
+
+print("Loading data...")
+traindev_folds = {}
+for line in open(pathlib.Path.home() / 'data' / 'autext' / 'data' / task / language / 'train_5folds.tsv'):
+    parts = line.strip().split('\t')
+    if len(parts) == 2:
+        traindev_folds[parts[0]] = parts[1]
+
+train_text = []
+train_Y = []
+dev_text = []
+dev_Y = []
+path = pathlib.Path.home() / 'data' / 'autext' / 'data' / task / language / 'train.tsv'
+for i, line in enumerate(open(path)):
+    parts = line.strip().split('\t')
+    sentence = parts[1]
+    if sentence == 'text':
+        continue
+    Y = None
+    if task == 'subtask_1':
+        if parts[2] == 'generated':
+            Y = 1
+        elif parts[2] == 'human':
+            Y = 0
+    if task == 'subtask_2':
+        if parts[2] == 'A':
+            Y = 0
+        elif parts[2] == 'B':
+            Y = 1
+        elif parts[2] == 'C':
+            Y = 2
+        elif parts[2] == 'D':
+            Y = 3
+        elif parts[2] == 'E':
+            Y = 4
+        elif parts[2] == 'F':
+            Y = 5
+    if traindev_folds[parts[0]] == '0':
+        dev_text.append(sentence)
+        dev_Y.append(Y)
+    else:
+        train_text.append(sentence)
+        train_Y.append(Y)
+    #if i > 1000:
+    #    break
+
+test_text = []
+test_ids = []
+testpath = pathlib.Path.home() / 'data' / 'autext' / 'data' / task / language / 'test.tsv'
+for i, line in enumerate(open(testpath)):
+    parts = line.strip().split('\t')
+    sentence = parts[1]
+    if sentence == 'text':
+        continue
+    test_text.append(sentence)
+    test_ids.append(parts[0])
+    #if i > 1000:
+    #    break
+
+train_Y = np.array(train_Y)
+dev_Y = np.array(dev_Y)
+
+print("Loaded data with " + str(len(train_Y) + len(dev_Y)) + " instances.")
+
+# Preparing feature generators
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+local_device = torch.device('cpu')
+
+perp = ProbabilisticFeatures(device, local_device, language, disable_sequence)
+# gram = GrammarFeatures(device, local_device, language)
+# freq = WordFrequency(device, local_device, language)
+feature_generators = [perp]
+
+# print("Generating text derivations...")
+# text_derivator = TextDerivator(language, device, path.parent / 'train-derived.tsv')
+# derivations = text_derivator.derive(all_text)
+
+print("Generating sequence features...")
+train_X = []
+dev_X = []
+test_X = []
+for in_text, out_X in zip([train_text, dev_text, test_text], [train_X, dev_X, test_X]):
+    for feature_generator in feature_generators:
+        out_X.append(np.array(feature_generator.word_features(in_text)))
+train_X = np.concatenate(train_X, axis=2)
+dev_X = np.concatenate(dev_X, axis=2)
+test_X = np.concatenate(test_X, axis=2)
+
+print("Tokenising text for RoBERTa...")
+tokenizer = RobertaTokenizer.from_pretrained(roberta_variant)
+train_encodings = tokenizer(train_text, padding=True, truncation=True, max_length=fixed_len, return_tensors="pt")
+dev_encodings = tokenizer(dev_text, padding=True, truncation=True, max_length=fixed_len, return_tensors="pt")
+test_encodings = tokenizer(test_text, padding=True, truncation=True, max_length=fixed_len, return_tensors="pt")
+
+# CUDA memory cleaning
+if torch.cuda.is_available():
+    torch.cuda.empty_cache()
+    print(str(torch.cuda.get_device_properties(0).total_memory))
+    print(str(torch.cuda.memory_reserved(0)))
+    print(str(torch.cuda.memory_allocated(0)))
+
+train_input_ids = train_encodings['input_ids']
+train_attention_mask = train_encodings['attention_mask']
+
+dev_input_ids = dev_encodings['input_ids']
+dev_attention_mask = dev_encodings['attention_mask']
+
+test_input_ids = test_encodings['input_ids']
+test_attention_mask = test_encodings['attention_mask']
+
+print("Building a model...")
+BATCH_SIZE = 16
+train_dataset = TensorDataset(torch.tensor(train_X).float(), train_input_ids, train_attention_mask,
+                              torch.tensor(np.array(train_Y)).long())
+dev_dataset = TensorDataset(torch.tensor(dev_X).float(), dev_input_ids, dev_attention_mask,
+                            torch.tensor(np.array(dev_Y)).long())
+test_dataset = TensorDataset(torch.tensor(test_X).float(), test_input_ids, test_attention_mask,
+                             torch.tensor(np.zeros(len(test_text))).long())
+train_loader = DataLoader(train_dataset, shuffle=True, batch_size=BATCH_SIZE)
+dev_loader = DataLoader(dev_dataset, shuffle=True, batch_size=BATCH_SIZE)
+test_loader = DataLoader(test_dataset, shuffle=False, batch_size=BATCH_SIZE)
+
+if model_type == 'BiLSTM':
+    model = BiLSTM(train_X.shape[2], task, local_device).to(device)
+elif model_type == 'Hybrid':
+    model = HybridBiLSTMRoBERTa(train_X.shape[2], task, local_device, roberta_variant, disable_sequence).to(device)
+
+print("Preparing training")
+model = model.to(device)
+learning_rate = 1e-3
+optimizer = Adam(model.parameters(), lr=learning_rate)
+milestones = [5] if model_type == 'Hybrid' else []
+scheduler = MultiStepLR(optimizer, milestones=milestones, gamma=0.02)
+skip_visual = False
+stats_file = open(pathlib.Path.home() / 'data' / 'autext' / 'out' / (task + '_' + language + '_stats.tsv'),
+                  'w')
+stats_file.write('epoch\ttrain_F1\tdev_F1\n')
+
+eval_loop(dev_loader, model, device, local_device, skip_visual)
+for epoch in range(20):
+    print("EPOCH " + str(epoch + 1))
+    if model_type == 'Hybrid':
+        if epoch < 5:
+            model.freeze_llm()
+        else:
+            model.unfreeze_llm()
+    train_f1 = train_loop(train_loader, model, optimizer, scheduler, device, local_device, skip_visual)
+    dev_f1 = eval_loop(dev_loader, model, device, local_device, skip_visual, test=False)
+    test_preds = eval_loop(test_loader, model, device, local_device, skip_visual, test=True)
+    stats_file.write(str(epoch + 1) + '\t' + str(train_f1) + '\t' + str(dev_f1) + '\n')
+    with open(pathlib.Path.home() / 'data' / 'autext' / 'out' / (
+            task + '_' + language + '_preds_' + str(epoch + 1) + '.tsv'), 'w') as f:
+        f.write('id\tlabel\n')
+        for test_id, pred in zip(test_ids, test_preds):
+            label = ['human', 'generated'][pred] if task == 'subtask_1' else ['A', 'B', 'C', 'D', 'E', 'F'][pred]
+            f.write(test_id + '\t' + label + '\n')
+
+stats_file.close()
+print("The end!")
--- models/training.py
+++ models/training.py
@@ -1,18 +1,24 @@
 import torch
+from sklearn.metrics import f1_score
 from tqdm.auto import tqdm
 import numpy as np
+
 
 def train_loop(dataloader, model, optimizer, scheduler, device, local_device, skip_visual=False):
     print("Training...")
     model.train()
     progress_bar = tqdm(range(len(dataloader)), ascii=True, disable=skip_visual)
     losses = []
+    preds = []
+    true_Y = []
     for XY in dataloader:
         XY = [xy.to(device) for xy in XY]
         Xs = XY[:-1]
         Y = XY[-1]
-        pred = model(*Xs)
-        loss = model.compute_loss(pred, Y)
+        raw_pred = model(*Xs)
+        preds.append(model.postprocessing(raw_pred))
+        true_Y.append(Y.to(local_device).numpy())
+        loss = model.compute_loss(raw_pred, Y)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -20,18 +26,23 @@ def train_loop(dataloader, model, optimizer, scheduler, device, local_device, sk
         progress_bar.update(1)
     scheduler.step()
     print('Train loss: ' + str(np.mean(losses)))
+    preds = np.concatenate(preds)
+    true_Y = np.concatenate(true_Y)
+    f1 = f1_score(y_true=true_Y, y_pred=preds, average="macro")
+    return f1
 
 
-def eval_loop(dataloader, model, device, local_device, skip_visual=False):
-    print("Evaluating...")
+def eval_loop(dataloader, model, device, local_device, skip_visual=False, test=False):
+    if test:
+        print("Generating predictions...")
+    else:
+        print("Evaluating...")
     model.eval()
     progress_bar = tqdm(range(len(dataloader)), ascii=True, disable=skip_visual)
     correct = 0
     size = 0
-    TPs = 0
-    FPs = 0
-    FNs = 0
     preds = []
+    true_Y = []
     with torch.no_grad():
         for XY in dataloader:
             XY = [xy.to(device) for xy in XY]
@@ -40,14 +51,17 @@ def eval_loop(dataloader, model, device, local_device, skip_visual=False):
             pred = model.postprocessing(model(*Xs))
             preds.append(pred)
             Y = Y.numpy()
+            true_Y.append(Y)
             eq = np.equal(Y, pred)
             size += len(eq)
             correct += sum(eq)
-            TPs += sum(np.logical_and(np.equal(Y, 1.0), np.equal(pred, 1.0)))
-            FPs += sum(np.logical_and(np.equal(Y, 0.0), np.equal(pred, 1.0)))
-            FNs += sum(np.logical_and(np.equal(Y, 1.0), np.equal(pred, 0.0)))
             progress_bar.update(1)
-    print('Accuracy: ' + str(correct / size))
-    # print('Binary F1: ' + str(2 * TPs / (2 * TPs + FPs + FNs)))
     preds = np.concatenate(preds)
-    return (preds)
+    true_Y = np.concatenate(true_Y)
+    if not test:
+        print('Accuracy: ' + str(correct / size))
+        f1 = f1_score(y_true=true_Y, y_pred=preds, average="macro")
+        print('F1 score: ' + str(f1))
+        return f1
+    else:
+        return preds
